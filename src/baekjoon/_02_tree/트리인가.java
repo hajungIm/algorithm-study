d68227c3c935054b8fd506ee117d51a41eb192d0
hajungIm <ihj_ewha@ewhain.net>
2024-03-02 23:04:10 +0900

    240302

--- src/baekjoon/_02_tree/트리인가.java
+++ src/baekjoon/_02_tree/트리인가.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class 트리인가 {
     public static void main(String[] args) {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 
 }
