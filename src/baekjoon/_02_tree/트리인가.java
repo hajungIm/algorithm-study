79c19885c4652f303436106b141ce362c8f146fe
hajungIm <ihj_ewha@ewhain.net>
2024-03-08 18:00:10 +0900

    240308

--- src/baekjoon/_02_tree/트리인가.java
+++ src/baekjoon/_02_tree/트리인가.java
@@ -8,9 +8,73 @@ import java.util.*;
  */
 
 public class 트리인가 {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean isEnd = false;
 
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
+
+        while (true) {
+            ArrayList<int[]> input = getInput();
+            if (isEnd) {
+                break;
+            }
+
+            if (isTree(input)) {
+                sb.append(String.format("Case %d is a tree.", cnt++)).append('\n');
+            } else {
+                sb.append(String.format("Case %d is not a tree.", cnt++)).append('\n');
+            }
+        }
+
+        System.out.println(sb);
     }
 
+    private static ArrayList<int[]> getInput() throws IOException {
+        ArrayList<int[]> result = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (true) {
+            while (!st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            if (u == 0) {
+                break;
+            }
+            if (u == -1) {
+                isEnd = true;
+                break;
+            }
+
+            result.add(new int[]{u, v});
+        }
+        return result;
+    }
+
+    private static boolean isTree(ArrayList<int[]> inputs) {
+        if (inputs.size() == 0) {
+            return true;
+        }
+
+        Set<Integer> income = new HashSet<>();
+        Map<Integer, Integer> line = new HashMap<>();
+        for (int[] input : inputs) {
+            // 루트 제외 모든 노드 단 하나의 들어오는 간선
+            if (!income.add(input[1])) {
+                return false;
+            }
+            line.put(input[0], line.getOrDefault(input[0], 0) + 1);
+            line.put(input[1], line.getOrDefault(input[0], 0));
+        }
+        // 전체 노드 수 - 1 == 간선 수 만족해야지 트리
+        if (line.size() - 1 == income.size()) {
+            return true;
+        }
+        return false;
+    }
 }
