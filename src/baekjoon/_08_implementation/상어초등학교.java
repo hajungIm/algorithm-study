64b774dc92075742ac46500018fc982aaa19a9fe
hajungIm <ihj_ewha@ewhain.net>
2024-03-20 23:29:39 +0900

    240320

--- src/baekjoon/_08_implementation/상어초등학교.java
+++ src/baekjoon/_08_implementation/상어초등학교.java
@@ -22,6 +22,18 @@ public class 상어초등학교 {
             this.emptyCnt = emptyCnt;
         }
 
+        public Seat compare(Seat seat) {
+            if (this.likedCnt > seat.likedCnt) {
+                return this;
+            } else if (this.emptyCnt > seat.emptyCnt) {
+                return this;
+            } else if (this.x < seat.x) {
+                return this;
+            } else if (this.y < seat.y) {
+                return this;
+            }
+            return seat;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,6 +50,15 @@ public class 상어초등학교 {
                 liked.get(student[i]).add(Integer.parseInt(st.nextToken()));
             }
         }
+
+        int[][] map = new int[N * N][N * N];
+
+        for (int i = 0; i < N * N; i++) {
+            Seat seat = findSeat(students[i]);
+        }
+    }
+
+    private static Seat findSeat(int student) {
 
     }
 }
