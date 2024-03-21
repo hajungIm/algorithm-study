41b643c3b42a8aff1b75fbfc11842d3d81fb2478
hajungIm <ihj_ewha@ewhain.net>
2024-03-21 23:28:47 +0900

    240321

--- src/baekjoon/_08_implementation/상어초등학교.java
+++ src/baekjoon/_08_implementation/상어초등학교.java
@@ -40,14 +40,14 @@ public class 상어초등학교 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] student = new int[N * N];
+        int[] students = new int[N * N];
         HashMap<Integer, Set<Integer>> liked = new HashMap<>();
         for (int i = 0; i < N * N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            student[i] = Integer.parseInt(st.nextToken());
-            liked.put(student[i], new HashSet<>());
+            students[i] = Integer.parseInt(st.nextToken());
+            liked.put(students[i], new HashSet<>());
             for (int j = 0; j < 4; j++) {
-                liked.get(student[i]).add(Integer.parseInt(st.nextToken()));
+                liked.get(students[i]).add(Integer.parseInt(st.nextToken()));
             }
         }
 
